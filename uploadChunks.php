5c3c02f1bcb2030784435dcdb91772a7469fea49
Enmanuel M <emijacobo@gmail.com>
2018-09-05 11:30:21 -0500

    Ajustes a uploadChunks, subida sin hacer move() previo, retornando URL view con id del archivo

--- uploadChunks.php
+++ uploadChunks.php
@@ -5,27 +5,21 @@ require_once "drive_base.php";
 $client = getClient();
 $service = new Google_Service_Drive($client);
 
-$currentDir = getcwd();
-$uploadDirectory = "/uploads/";
-
 $fileName = $_FILES['fileToUpload']['name'];
 $fileSize = $_FILES['fileToUpload']['size'];
 $fileTmpName  = $_FILES['fileToUpload']['tmp_name'];
 $fileType = $_FILES['fileToUpload']['type'];
-$fileExtension = strtolower(end(explode('.',$fileName)));
+//$fileExtension = strtolower(end(explode('.',$fileName)));
 
-$uploadPath = $currentDir . $uploadDirectory . uniqid() . ".". $fileExtension; 
+$file = new Google_Service_Drive_DriveFile(array(
+  'name' => $fileName,
+  'parents' => array('12WY926dH27SFPpXgaxczkXyeW_64tvy0')));
 
-  $file = new Google_Service_Drive_DriveFile(array(
-    'name' => $fileName,
-    'parents' => array('12WY926dH27SFPpXgaxczkXyeW_64tvy0')));
+$chunkSizeBytes = 1 * 1024 * 1024;
 
-  $chunkSizeBytes = 1 * 1024 * 1024;
-  //$chunkSizeBytes = 524288;
-  
-  // Call the API with the media upload, defer so it doesn't immediately return.
-  $client->setDefer(true);
-  $request = $service->files->create($file);
+// Call the API with the media upload, defer so it doesn't immediately return.
+$client->setDefer(true);
+$request = $service->files->create($file, array('fields' => 'id'));
 
 // Create a media file upload to represent our upload process.
 $media = new Google_Http_MediaFileUpload(
@@ -36,35 +30,43 @@ $media = new Google_Http_MediaFileUpload(
     true,
     $chunkSizeBytes
 );
-$media->setFileSize($_FILES['fileToUpload']['size']);
+$media->setFileSize($fileSize);
 
-  // Upload the various chunks. $status will be false until the process is
-  // complete.
-  $status = false;
-  $didUpload = move_uploaded_file($fileTmpName, $uploadPath);
-  $handle = null;
+// Upload the various chunks. $status will be false until the process is complete.
+$status = false;
+$handle = fopen($fileTmpName, "rb");
 
-  if ($didUpload){ 
-      $handle = fopen($uploadPath, "rb");
-  }
+while (!$status && !feof($handle)) {
+  // read until you get $chunkSizeBytes from TESTFILE
+  // fread will never return more than 8192 bytes if the stream is read buffered and it does not represent a plain file
+  // An example of a read buffered file is when reading from a URL
+  //$chunk = readVideoChunk($handle, $chunkSizeBytes);
+  $resumeUri = $media->getResumeUri();
+  $chunk = fread($handle, $chunkSizeBytes);
+  $status = $media->nextChunk($chunk);
 
-  while (!$status && !feof($handle)) {
-    // read until you get $chunkSizeBytes from TESTFILE
-    // fread will never return more than 8192 bytes if the stream is read buffered and it does not represent a plain file
-    // An example of a read buffered file is when reading from a URL
-    //$chunk = readVideoChunk($handle, $chunkSizeBytes);
-    $chunk = fread($handle, $chunkSizeBytes);
-    $status = $media->nextChunk($chunk);
-  }
-  // The final value of $status will be the data from the API for the object
-  // that has been uploaded.
-  $result = false;
-  if ($status != false) {
-    $result = $status;
-  }
-  fclose($handle);
-  unlink($uploadPath);
+  /*if(!$status){ //nextChunk() returns 'false' whenever the upload is still in progress
+    echo 'sucessfully uploaded file up to byte ' . $media->getProgress() . 
+    ' which is ' . ( $media->getProgress() / $chunkSizeBytes ) . '% of the whole file';
+  }*/
 
+}
+
+// The final value of $status will be the data from the API for the object
+// that has been uploaded.
+fclose($handle);
+
+$result = false;
+if ($status != false) {
+  $result = $status;
+  $response = array('success' => true, 'message' => "https://drive.google.com/file/d/". $result->id ."/view" );
+  echo json_encode($response);
+  exit;
+}
+
+$response = array('success' => false, 'message' => "error" );
+echo json_encode($response);
+//unlink($uploadPath);
 
 function readVideoChunk ($handle, $chunkSize)
 {
